38310c9fb36e4c0693a74eaedad3cb5ed39c35c2
BasiliskDev <99629378+BasiliskDev@users.noreply.github.com>
2026-06-24 16:21:40 +0800

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = [0] * 26
+        for task in tasks:
+            freq[ord(task) - ord('A')] += 1
+        freq.sort()
+        maxf = freq.pop()
+
+        idle_time = (maxf-1)*n
+
+        for f in freq:
+            idle_time -= min(maxf-1, f)
+        
+        return max(0, idle_time) + len(tasks)
